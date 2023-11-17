72e004ad66a7f76cdb381b209e8a0248f3a9af03
Chu Đình Hiển <hienc1212@gmail.com>
2023-11-18 03:19:09 +0700

    Đẩy file tĩnh lên cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -131,10 +131,6 @@ module.exports.createPost = async (req,res) => {
     req.body.position = parseInt(req.body.position)
   }
 
-  if(req.file){
-    req.body.thumbnail = `/uploads/${req.file.filename}`
-  }
-  
   const product = new Product(req.body)
   await product.save()
   res.redirect(`${systemConfig.prefixAdmin}/products`)
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,38 +1,40 @@
 const express = require('express')
 const multer = require('multer')
 const router = express.Router()
-const storageMulter = require('../../helpers/storageMulter')
-const upload = multer({ storage: storageMulter() })
+
+const upload = multer()
 
 const controller = require('../../controllers/admin/product.controller')
 const validate = require('../../validates/admin/product.validate')
+const uploadCloud = require('../../middlewares/admin/uploadCloud.middleware')
 
 router.get('/', controller.index)
 
 router.patch('/change-status/:status/:id', controller.changeStatus)
 
-router.patch('/change-multi',controller.changeMulti)
+router.patch('/change-multi', controller.changeMulti)
 
-router.delete('/delete/:id',controller.deleteItem)
+router.delete('/delete/:id', controller.deleteItem)
 
 router.get('/create', controller.create)
 
 router.post(
-  '/create', 
+  '/create',
   upload.single('thumbnail'),
-  validate.createPost, 
+  uploadCloud.upload,
+  validate.createPost,
   controller.createPost
 )
 
-router.get('/edit/:id',controller.edit)
+router.get('/edit/:id', controller.edit)
 
 router.patch(
   '/edit/:id',
   upload.single('thumbnail'),
-  validate.createPost, 
+  validate.createPost,
   controller.editPatch
 )
 
-router.get('/detail/:id',controller.detail)
+router.get('/detail/:id', controller.detail)
 
 module.exports = router
